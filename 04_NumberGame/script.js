db03057b2117f4bb1e5cbc9b4ded598785bfc7dc
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-16 14:07:09 +0530

    c

---
+++ 04_NumberGame/script.js
@@ -0,0 +1,19 @@
+function valid(){
+
+}
+function checkGuess(){
+
+}
+function displayGuess(){
+
+}
+function displayMessage(){
+
+}
+function endGame(){
+
+}
+function newGame(){
+
+}
+let number = Math.random()*100
